b75c6cb8f9fc032dc86ba5cd866f4de9faf492ba
Milly Ndeto <milkandeto08@gmail.com>
2024-10-30 18:49:16 +0300

    Resolved merge conflict in protected.js

--- routes/protected.js
+++ routes/protected.js
@@ -14,8 +14,4 @@ router.get('/profile', requireAuth, (req, res) => {
   res.render('protected/profile', { user: req.user });
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 175b4b9177e37629d0233d824fa33d6106934471
